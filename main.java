5b76b9bca1846bc8214ff062ccbeb6672c5c73c4
HARSH PATEL <110300395+harshh100@users.noreply.github.com>
2022-09-12 22:27:23 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+import calc.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        sum obj1 = new sum();
+        System.out.println("sum =" + obj1.sum(20, 10));
+        sub obj2 = new sub();
+        System.out.println("sub =" + obj2.sub(20, 10));
+        mul obj3 = new mul();
+        System.out.println("mul =" + obj3.mul(20, 10));
+        div obj4 = new div();
+        System.out.println("div =" + obj4.div(20, 10));
+        System.out.println("Created by Harsh Patel  ID NO. 21CE090");
+
+    }
+
+}
